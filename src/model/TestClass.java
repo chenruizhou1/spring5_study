0180c3e65381ce139dade85572e1829403fbe603
chenruizhou <1259522407@qq.com>
2021-08-19 16:03:29 +0800

    2021/8/19 C

--- src/model/TestClass.java
+++ src/model/TestClass.java
@@ -1,6 +1,5 @@
 package model;
 
-import com.mysql.jdbc.log.Log;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
